619ef482e714a2c6228b0b212c81fbd0ffe158e0
Kenio Natan <kenionatan@gmail.com>
2019-03-05 20:37:34 -0300

    Get restaurants json list from http service

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,27 +1,19 @@
+import { Injectable } from '@angular/core';
 import { Restaurant } from "./restaurant/restaurant.model";
+import { MEAT_API } from '../app.api';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { ErrorHandler } from '../app.error-handler';
 
+@Injectable()
 export class RestaurantsService {
-    constructor(){}
+    constructor(private http:Http){}
 
-    rests: Restaurant[] = [
-        {
-          id: "bread-bakery",
-          name: "Bread & Bakery",
-          category: "Bakery",
-          deliveryEstimate: "25m",
-          rating: 4.9,
-          imagePath: "assets/img/restaurants/breadbakery.png"
-        },
-        {
-          id: "burger-house",
-          name: "Burger House",
-          category: "Hamburgers",
-          deliveryEstimate: "100m",
-          rating: 3.5,
-          imagePath: "assets/img/restaurants/burgerhouse.png"
-        }]
-
-    restaurants(): Restaurant[] {
-        return this.rests;
+    restaurants(): Observable<Restaurant[]> {
+        return this.http.get(`${MEAT_API}/restaurants`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
     }
 }
