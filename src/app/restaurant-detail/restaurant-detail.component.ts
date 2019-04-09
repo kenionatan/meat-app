5c587dbeaebb7227f726d2b6d1c484bb6cd87109
Kenio Natan <kenionatan@gmail.com>
2019-04-08 21:51:13 -0300

    Restaurant detail: returning header from API

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RestaurantsService } from 'app/restaurants/restaurants.service';
+import { Restaurant } from 'app/restaurants/restaurant/restaurant.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mt-restaurant-detail',
@@ -6,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private restaurantsService: RestaurantsService, 
+              private route: ActivatedRoute) { }
+
+  restaurant: Restaurant
 
   ngOnInit() {
+    this.restaurantsService.restaurantById(this.route.snapshot.params['id'])
+      .subscribe(restaurant => this.restaurant = restaurant)
   }
 
 }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -16,4 +16,10 @@ export class RestaurantsService {
             .map(response => response.json())
             .catch(ErrorHandler.handleError)
     }
+
+    restaurantById(id: string): Observable<Restaurant>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
 }
